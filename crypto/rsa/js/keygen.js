15a446565e7542d00602c2c1cb02445711a219ed
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-10-12 16:28:25 -0400

    Update keygen.js

--- crypto/rsa/js/keygen.js
+++ crypto/rsa/js/keygen.js
@@ -3,6 +3,9 @@
 // Generate 2048 bit prime number:
 
 // Helpers:
-function fastExpMod(base, exp, mod) {
-  
+function fastExpMod(base, exp, mod) {// parameters are BinInts
+  // Convert exp into bit list to use square and multiply algorithm
+  let expBinStr=exp.toString(2);
+  // Implimentation:
+  let res=BigInt(1);
 }
