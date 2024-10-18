c309c2a3d88f2fb93584fc0f411fb195b1eb952a
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-10-18 17:18:11 -0400

    Update keygen.js

--- crypto/rsa/js/keygen.js
+++ crypto/rsa/js/keygen.js
@@ -1,6 +1,6 @@
 // Open source RSA keygen software for web workers
 
-self.onmessage=function(code){eval(code);};
+self.onmessage=function(code){eval(code.data);};
 
 // Generate 2048 bit prime number:
 
