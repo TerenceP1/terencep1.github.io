e2a8fdd555ba7fc35985fde56bd270e70abe99ea
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2024-10-21 20:00:51 -0400

    Update aes.js

--- crypto/aes/aes.js
+++ crypto/aes/aes.js
@@ -1,1 +1,7 @@
 // Web worker that implements AES256
+
+function aes(
+  inp, // Uint8Array length 16 representing input
+  key, // Uint8Array length 32 representing key
+  mode // false if decryting, true if encrypting
+){}
