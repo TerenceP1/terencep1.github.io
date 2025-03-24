0374cbdece0899f624bb544c23de8515d2da17df
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-24 15:15:06 -0400

    Update sandbox.js

--- unblocked/sandbox.js
+++ unblocked/sandbox.js
@@ -23,34 +23,8 @@ function(){
   window.Request=function(a,b=undefined){
     let frq;
     if (typeof a !== "string"){
-      this.body = a.body;readonly(this,"body");
-      this.bodyUsed = a.bodyUsed;readonly(this,"bodyUsed");
-      this.cache = a.cache;readonly(this,"cache");
-      this.credentials = a.credentials;readonly(this,"credentials");
-      this.destination = a.destination;readonly(this,"destination");
-      this.headers = a.headers;readonly(this,"headers");
-      this.integrity = a.integrity;readonly(this,"integrity");
-      this.isHistoryNavigation = a.isHistoryNavigation;readonly(this,"isHistoryNavigation");
-      this.keepalive = a.keepalive;readonly(this,"keepalive");
-      this.method = a.method;readonly(this,"method");
-      this.redirect = a.redirect;readonly(this,"redirect");
-      this.referrer = a.referrer;readonly(this,"referrer");
-      this.referrerPolicy = a.referrerPolicy;readonly(this,"referrerPolicy");
-      this.signal = a.signal;readonly(this,"signal");
-      this.url = a.url;readonly(this,"url");
-      this.mode = a.mode;
-      if (a.mode === "navigate"){
-        this.mode="same-origin";
-      }
-      readonly(this,"mode");
-      this.arrayBuffer = a.arrayBuffer;
-      this.blob = a.blob;
-      this.bytes = a.bytes;
-      this.clone = a.clone;
-      this.formData = a.formData;
-      this.json = a.json;
-      this.text = a.text;
-      return this;
+      
+      return new oRequest(a);
     }
     else{
       let proxied=proxynate(a);
@@ -61,30 +35,10 @@ function(){
         frq=new oRequest(proxied.url,b);
       }
       
-      this.body = frq.body;readonly(this,"body");
-      this.bodyUsed = frq.bodyUsed;readonly(this,"bodyUsed");
-      this.cache = frq.cache;readonly(this,"cache");
-      this.credentials = frq.credentials;readonly(this,"credentials");
-      this.destination = frq.destination;readonly(this,"destination");
-      this.headers = frq.headers;readonly(this,"headers");
-      this.integrity = frq.integrity;readonly(this,"integrity");
-      this.isHistoryNavigation = frq.isHistoryNavigation;readonly(this,"isHistoryNavigation");
-      this.keepalive = frq.keepalive;readonly(this,"keepalive");
-      this.method = frq.method;readonly(this,"method");
-      this.mode = frq.mode;readonly(this,"mode");
-      this.redirect = frq.redirect;readonly(this,"redirect");
-      this.referrer = frq.referrer;readonly(this,"referrer");
-      this.referrerPolicy = frq.referrerPolicy;readonly(this,"referrerPolicy");
-      this.signal = frq.signal;readonly(this,"signal");
-      this.url = frq.url;readonly(this,"url");
-      this.arrayBuffer = frq.arrayBuffer;
-      this.blob = frq.blob;
-      this.bytes = frq.bytes;
-      this.clone = frq.clone;
-      this.formData = frq.formData;
-      this.json = frq.json;
-      this.text = frq.text;
-      return this;
+      delete frq.url;
+      frq.url=proxied.oUrl;
+      readonly(frq.url)
+      return frq;
     }
   }
 }();
