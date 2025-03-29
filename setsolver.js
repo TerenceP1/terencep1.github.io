f1a746a07b82c8bb57294133d1f5c4ffa535fe8b
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-29 16:20:02 -0400

    Update setsolver.js

--- setsolver.js
+++ setsolver.js
@@ -4,3 +4,16 @@
 // Shapes: 0=oval, 1=rhombus, 2=squigle
 // color: 0=red, 1=green, 2=purple
 // filling: 0=none, 1=partial, 2=full
+// [shape,color,filling]
+
+function setsolve(a){
+  len=a.length;
+  for (let i=0;i<len;i++){
+    for (let j=i+1;j<len;j++){
+      for (let k=j+1;k<len;k++){
+        if (!((a[i][0]==a[j][0] && a[i][0]==a[k][0])||
+              (a[i][0]!=a[j][0] && a[i][0]!=a[k][0]
+      }
+    }
+  }
+}
