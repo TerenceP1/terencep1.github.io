7de63953fe1791077f94b5d741c1f0c24ea70477
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-29 16:46:05 -0400

    Update setsolver.js

--- setsolver.js
+++ setsolver.js
@@ -13,6 +13,20 @@ function setsolve(a){
       for (let k=j+1;k<len;k++){
         if (!((a[i][0]==a[j][0] && a[i][0]==a[k][0])||
               (a[i][0]!=a[j][0] && a[i][0]!=a[k][0]
+               && a[j][0]!=a[k][0]))){
+          continue;
+        }
+        if (!((a[i][1]==a[j][1] && a[i][1]==a[k][1])||
+              (a[i][1]!=a[j][1] && a[i][1]!=a[k][1]
+               && a[j][1]!=a[k][1]))){
+          continue;
+        }
+        if (!((a[i][2]==a[j][2] && a[i][2]==a[k][2])||
+              (a[i][2]!=a[j][2] && a[i][2]!=a[k][2]
+               && a[j][2]!=a[k][2]))){
+          continue;
+        }
+        alert("SET FOUND!!!!!!"+[i,j,k].toString());
       }
     }
   }
