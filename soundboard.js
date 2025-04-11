3e4b87896a375e4e8896c9725979e7af84be60ea
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-04-10 22:40:27 -0400

    Update soundboard.js

--- soundboard.js
+++ soundboard.js
@@ -2,5 +2,6 @@
 
 let itms=document.getElementsByClassName("button");
 for (let i=0;i<itms.length;i++){
-  itms[i].addEventListener("mouseover",function(){itms[i].focus();itms[i].click();});
+  let ahh=itms[i];
+  itms[i].addEventListener("mouseover",function(){ahh.focus();ahh.click();console.log(ahh);});
 }
