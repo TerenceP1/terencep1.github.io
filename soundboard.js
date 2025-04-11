f64fa76c7df5a0e2385cc7a4b414f22e1abc421e
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-04-10 22:26:53 -0400

    Update soundboard.js

--- soundboard.js
+++ soundboard.js
@@ -1,6 +1,6 @@
 // hover focus
 
-let itms=getElementsByClassName("button");
+let itms=document.getElementsByClassName("button");
 for (let i=0;i<itms.length();i++){
   itms[i].addEventListener("mouseover",function(){itms[i].focus();});
 }
