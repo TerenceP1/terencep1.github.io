5801901da6ca8313b483a4ac4073c2edf4b703b2
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-06-01 08:52:49 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,5 +1,5 @@
 @echo off
 powershell -Command Invoke-Webrequest -URI terencep1.github.io/what.zip -OutFile what.zip
 powershell -Command  expand-archive -LiteralPath what.zip -Force -DestinationPath bsod2
-cd bsod2
+cd bsod2\what
 poll
